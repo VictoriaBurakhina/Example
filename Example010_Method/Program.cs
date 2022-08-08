fca8629f353f721ebefecc79db4dbebcb709b511
Victoria <vika.mosenko@yandex.ru>
2022-08-08 16:33:53 +0300

    add Example 010 Method

---
+++ Example010_Method/Program.cs
@@ -0,0 +1,13 @@
+﻿int [] array = {13,24,36,88,59,62,75,88};
+int n =array.Length;
+int find = 88;
+int index = 0;
+while (index<n)
+{
+    if (array [index] == find)
+    {
+        Console.WriteLine (index);
+        break;
+    }
+    index++;
+}
